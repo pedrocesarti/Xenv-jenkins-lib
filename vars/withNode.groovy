581e46cd8d499255841f53691d3b34891a54fa09
Pedro César de Azevedo <pedrocesar.ti@gmail.com>
2018-09-22 05:51:52 +1200

    Adding logic

--- vars/withNode.groovy
+++ vars/withNode.groovy
@@ -1,8 +1,18 @@
 #!/usr/bin/env groovy
 
-def call(version='6.14.4', closure) {
+def call(version='6.14.4', cl) {
 
   echo "Setting up NodeJS version ${version}!"
   
-  
+  if (!fileExists("${JENKINS_HOME}/.nodenv/bin/nodenv")) {
+    install()
+  } else {
+    echo "Nodenv located!"
+  }
+
+  cl()
+}
+
+def install() {
+    echo "Lets install!!!"
 }
