08457eaefc2278c9136a870e71e28790c09d64c0
Pedro César de Azevedo <pedrocesar.ti@gmail.com>
2018-09-23 21:36:41 +1200

    Fix bug to switch case

--- src/info/pedrocesar/utils.groovy
+++ src/info/pedrocesar/utils.groovy
@@ -4,20 +4,21 @@ import com.cloudbees.groovy.cps.NonCPS
 
 @NonCPS
 def installMetarunner(String metarunner){
-  sh "git clone https://github.com/${metarunner}/${metarunner}.git ${JENKINS_HOME}/.${metarunner}"
-  print "I GOT HERE"
 
-  if (metarunner != 'pyenv') {
-    print "It's not python"
-    if (metarunner == 'nodenv') {
-      print "its NODE"
-      sh "git clone https://github.com/${metarunner}/node-build.git ${JENKINS_HOME}/.${metarunner}/plugins/node-build"
-    } else if (metarunner == 'rbenv') {
-      print "It's Ruby"
-      sh "git clone https://github.com/${metarunner}/ruby-build.git ${JENKINS_HOME}/.${metarunner}/plugins/ruby-build"
-    } else {
-      print "no metarunner found it!"    
-    }
+  sh "git clone https://github.com/${metarunner}/${metarunner}.git ${JENKINS_HOME}/.${metarunner}"
+  sh "git clone https://github.com/${metarunner}/node-build.git ${JENKINS_HOME}/.${metarunner}/plugins/node-build"
+
+//if (metarunner != 'pyenv') {
+//    print "It's not python"
+//    if (metarunner == 'nodenv') {
+//      print "its NODE"
+//      sh "git clone https://github.com/${metarunner}/node-build.git ${JENKINS_HOME}/.${metarunner}/plugins/node-build"
+//    } else if (metarunner == 'rbenv') {
+//      print "It's Ruby"
+//      sh "git clone https://github.com/${metarunner}/ruby-build.git ${JENKINS_HOME}/.${metarunner}/plugins/ruby-build"
+//    } else {
+//      print "no metarunner found it!"    
+//    }
   }
 
   dir ("${JENKINS_HOME}/.${metarunner}") {
